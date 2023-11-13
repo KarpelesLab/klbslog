a7c0d6b0b632aa7841fa980ba15bbe3be974598e
Mark Karpeles <magicaltux@gmail.com>
2023-11-13 21:23:00 +0900

    implement actual log upload

--- handler.go
+++ handler.go
@@ -2,15 +2,22 @@ package klbslog
 
 import (
 	"context"
+	"log"
 	"runtime"
 	"strconv"
+	"sync"
 	"time"
 
 	"log/slog"
+
+	"github.com/KarpelesLab/rest"
 )
 
 type SHandler struct {
-	opts *slog.HandlerOptions
+	opts  *slog.HandlerOptions
+	queue []map[string]string
+	qlk   sync.Mutex
+	qcd   *sync.Cond
 }
 
 func New(opts *slog.HandlerOptions) slog.Handler {
@@ -20,6 +27,11 @@ func New(opts *slog.HandlerOptions) slog.Handler {
 	res := &SHandler{
 		opts: opts,
 	}
+	res.qcd = sync.NewCond(&res.qlk)
+
+	// note: we can run this multiple times to have multiple parallel uploaders
+	go res.run()
+
 	return res
 }
 
@@ -66,5 +78,57 @@ func (s *SHandler) Handle(ctx context.Context, r slog.Record) error {
 		attrs["go.source.file"] = f.File
 		attrs["go.source"] = f.File + ":" + strconv.Itoa(f.Line)
 	}
+
+	s.append(attrs)
 	return nil
+}
+
+func (s *SHandler) append(v map[string]string) {
+	s.qlk.Lock()
+	defer s.qlk.Unlock()
+
+	s.queue = append(s.queue, v)
+	s.qcd.Broadcast()
+}
+
+func (s *SHandler) run() {
+	s.qlk.Lock()
+	defer s.qlk.Unlock()
+
+	// this runs in a separate goroutine
+	for {
+		if len(s.queue) == 0 {
+			// nothing in queue
+			s.qcd.Wait()
+			continue
+		}
+		// take queue
+		q := s.queue
+		s.queue = nil
+
+		// run it (lock will be released during runQueue)
+		s.runQueue(q)
+	}
+}
+
+func (s *SHandler) runQueue(q []map[string]string) {
+	// unlock qlk while running queue, but lock back afterward
+	s.qlk.Unlock()
+	defer s.qlk.Lock()
+
+	// let's just call the rest function SLog:append with logs=q
+	cnt := 0
+	for {
+		_, err := rest.Do(context.Background(), "SLog:append", "POST", map[string]any{"logs": q})
+		if err == nil {
+			// success
+			return
+		}
+		log.Printf("[klbslog] Failed to push logs: %s", err)
+		if cnt > 5 {
+			return
+		}
+		cnt += 1
+		time.Sleep(5 * time.Second)
+	}
 }
